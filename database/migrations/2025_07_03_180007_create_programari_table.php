20133f576282d2fc028680704f49cab4faa41eff
Ghidion Adrian <90002975+ghidion1@users.noreply.github.com>
2025-07-03 22:13:06 +0300

    Update 2025_07_03_180007_create_programari_table.php

--- database/migrations/2025_07_03_180007_create_programari_table.php
+++ database/migrations/2025_07_03_180007_create_programari_table.php
@@ -4,14 +4,14 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
+class CreateProgramariTable extends Migration
 {
-    public function up(): void
+    public function up()
     {
         Schema::create('programari', function (Blueprint $table) {
             $table->id();
-            $table->string('nume');
-            $table->string('prenume');
+            $table->string('nume', 64);
+            $table->string('prenume', 64);
             $table->string('specialitate');
             $table->string('medic');
             $table->date('data');
@@ -24,8 +24,8 @@ return new class extends Migration
         });
     }
 
-    public function down(): void
+    public function down()
     {
         Schema::dropIfExists('programari');
     }
-};
+}
