1702b5660640f9605cdeeefb53b3b7be26fcb242
Ghidion Adrian <90002975+ghidion1@users.noreply.github.com>
2025-07-03 21:54:57 +0300

    Create cors.php

---
+++ config/cors.php
@@ -0,0 +1,21 @@
+<?php
+
+return [
+
+    'paths' => ['api/*'],
+
+    'allowed_methods' => ['*'],
+
+    'allowed_origins' => ['https://emanuel-cioburciu.md'],
+
+    'allowed_origins_patterns' => [],
+
+    'allowed_headers' => ['*'],
+
+    'exposed_headers' => [],
+
+    'max_age' => 0,
+
+    'supports_credentials' => false,
+
+];
