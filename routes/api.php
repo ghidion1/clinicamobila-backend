eaf80127396f543cf56a8ca460cbcf5c05cbf6cf
Ghidion Adrian <90002975+ghidion1@users.noreply.github.com>
2025-07-03 22:27:38 +0300

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,11 @@
 <?php
-use App\Http\Controllers\ProgramareController;
 
-Route::post('/programari', [ProgramareController::class, 'store']);
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProgramareController;
 
 Route::get('/health', function () {
     return response()->json(['status' => 'ok']);
 });
+
+Route::post('/programari', [ProgramareController::class, 'store']);
